30850c89e8c88af617d5c5deed69122ef4de25d6
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-04-12 20:36:47 +0430

    Create html, css and js files and modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './style.css';
+import "./style.css";
+import scoresData from './modules/scoreData.js';
+
+scoresData.forEach((score) => {
+  const scoresList = document.getElementById('score-list');
+  const sC = `
+  <li class="score">
+    <span class="score-content">${score.name}: ${score.score}</span>
+  </li>
+  `;
+  scoresList.innerHTML += sC;
+});
