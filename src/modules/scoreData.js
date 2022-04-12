5000b5bc99e8af1d70d4f6090afaa9a083a0ea82
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-04-12 20:43:06 +0430

    create modules and hardcode array of objects

---
+++ src/modules/scoreData.js
@@ -0,0 +1,24 @@
+const scoresData = [
+    {
+      name: 'Name',
+      score: 100,
+    },
+    {
+      name: 'Name',
+      score: 20,
+    },
+    {
+      name: 'Name',
+      score: 40,
+    },
+    {
+      name: 'Name',
+      score: 32,
+    },
+    {
+      name: 'Name',
+      score: 18,
+    },
+  ];
+  
+  export default scoresData;
