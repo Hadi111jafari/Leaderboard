08cbd205b01f5223cfaad87c1bff9d69c341f7fe
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-04-14 16:04:17 +0430

    consume API (Send and receive data from an API.)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import './style.css';
-import scoresData from './modules/scoreData.js';
+// game id = WAUrjt2TEjdFGQcuCks4
+import "./style.css";
+import refreshBtn from "./modules/refresh.js";
+import scoresList from "./modules/scoreL.js";
+import submitData from "./modules/submit.js";
 
-scoresData.forEach((score) => {
-  const scoresList = document.getElementById('score-list');
-  const sC = `
-  <li class="score">
-    <span class="score-content">${score.name}: ${score.score}</span>
-  </li>
-  `;
-  scoresList.innerHTML += sC;
+const url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WAUrjt2TEjdFGQcuCks4/scores/";
+
+document.getElementById("submit-btn").addEventListener("click", submitData);
+
+const getScores = async () => {
+  const response = await fetch(url);
+  const result = await response.json();
+
+  for (const score of result.result) {
+    const sC = `
+    <li class="score">
+      <span class="score-content">${score.user}: ${score.score}</span>
+    </li>
+    `;
+    scoresList.innerHTML += sC;
+  }
+
+  return result;
+};
+
+getScores();
+
+refreshBtn.addEventListener("click", () => {
+  window.location.reload();
+  return false;
 });
